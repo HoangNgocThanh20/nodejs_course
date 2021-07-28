a9e6f9d09218883cd873d62312bbe7b212ca9eab
thanhhoang <maketwithxunu@gmail.com>
2021-07-28 23:05:04 +0700

    query param

--- src/index.js
+++ src/index.js
@@ -25,5 +25,9 @@ app.get('/news', (req, res) => {
   res.render('news')
 })
 
+app.get('/search', (req, res) => {
+  res.render('search')
+})
+
 
 app.listen(port, () => {console.log(`Example app listening at http://localhost:${port}`)})
