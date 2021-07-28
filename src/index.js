3d5b4b3a3db8368d01fd6788b8372b1a04683e7f
thanhhoang <maketwithxunu@gmail.com>
2021-07-28 21:56:53 +0700

    static file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const app = express();
 const port = 3000;
 const morgan = require('morgan');
 const handlebars  = require('express-handlebars');
+
+app.use(express.static(path.join(__dirname,'public')));
+
 // HTTP logger
 app.use(morgan('combined'))
 //Template engine
@@ -12,6 +15,7 @@ app.engine('hbs', handlebars({
 }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
+console.log(__dirname);
 
 app.get('/', (req, res) => {
   res.render('home')
