bb036df09bc78eb050c632ba7615c2c33b034cb0
thanhhoang <maketwithxunu@gmail.com>
2021-07-29 11:02:14 +0700

    router and controllers

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 3000;
 const morgan = require('morgan');
 const handlebars  = require('express-handlebars');
+const route = require('./routes');
 
 app.use(express.static(path.join(__dirname,'public')));
 
@@ -23,21 +24,7 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 console.log(__dirname);
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
+route(app);
 
-app.get('/news', (req, res) => {
-  res.render('news')
-})
-
-app.get('/search', (req, res) => {
-  res.render('search')
-})
-app.post('/search', (req, res) => {
-
-  console.log(req.body);
-  res.send('')
-})
 
 app.listen(port, () => {console.log(`Example app listening at http://localhost:${port}`)})
